590d830d6769831b5b948c628eb188c5bdf80733
Tyuiu.ChepurnykhSR@outlook.com <winxsofa@bk.ru>
2023-12-08 21:13:14 +0500

    Task5

--- Tyuiu.ChepurnykhSR.Sprint6.Task5.V16/FormMain.Designer.cs
+++ Tyuiu.ChepurnykhSR.Sprint6.Task5.V16/FormMain.Designer.cs
@@ -29,53 +29,200 @@ namespace Tyuiu.ChepurnykhSR.Sprint6.Task5.V16
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panelTop_CSR = new System.Windows.Forms.Panel();
+            this.panelLeft_CSR = new System.Windows.Forms.Panel();
+            this.panelRight_CSR = new System.Windows.Forms.Panel();
+            this.splitterPanel_CSR = new System.Windows.Forms.Splitter();
+            this.buttonHelp_CSR = new System.Windows.Forms.Button();
+            this.buttonFile_CSR = new System.Windows.Forms.Button();
+            this.buttonDone_CSR = new System.Windows.Forms.Button();
+            this.groupBoxTask_CSR = new System.Windows.Forms.GroupBox();
+            this.groupBoxResult_CSR = new System.Windows.Forms.GroupBox();
+            this.chartResult_CSR = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBoxTask_CSR = new System.Windows.Forms.TextBox();
+            this.dataGridViewResult_CRS = new System.Windows.Forms.DataGridView();
+            this.panelTop_CSR.SuspendLayout();
+            this.panelLeft_CSR.SuspendLayout();
+            this.panelRight_CSR.SuspendLayout();
+            this.groupBoxTask_CSR.SuspendLayout();
+            this.groupBoxResult_CSR.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartResult_CSR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResult_CRS)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // panelTop_CSR
             // 
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
-            this.panel1.TabIndex = 0;
+            this.panelTop_CSR.Controls.Add(this.groupBoxTask_CSR);
+            this.panelTop_CSR.Controls.Add(this.buttonDone_CSR);
+            this.panelTop_CSR.Controls.Add(this.buttonFile_CSR);
+            this.panelTop_CSR.Controls.Add(this.buttonHelp_CSR);
+            this.panelTop_CSR.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop_CSR.Location = new System.Drawing.Point(0, 0);
+            this.panelTop_CSR.Name = "panelTop_CSR";
+            this.panelTop_CSR.Size = new System.Drawing.Size(834, 74);
+            this.panelTop_CSR.TabIndex = 0;
             // 
-            // panel2
+            // panelLeft_CSR
             // 
-            this.panel2.Location = new System.Drawing.Point(12, 118);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 281);
-            this.panel2.TabIndex = 1;
+            this.panelLeft_CSR.Controls.Add(this.groupBoxResult_CSR);
+            this.panelLeft_CSR.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft_CSR.Location = new System.Drawing.Point(0, 74);
+            this.panelLeft_CSR.Name = "panelLeft_CSR";
+            this.panelLeft_CSR.Size = new System.Drawing.Size(200, 337);
+            this.panelLeft_CSR.TabIndex = 1;
             // 
-            // panel3
+            // panelRight_CSR
             // 
-            this.panel3.Location = new System.Drawing.Point(531, 188);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(200, 100);
-            this.panel3.TabIndex = 2;
+            this.panelRight_CSR.Controls.Add(this.chartResult_CSR);
+            this.panelRight_CSR.Controls.Add(this.splitterPanel_CSR);
+            this.panelRight_CSR.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRight_CSR.Location = new System.Drawing.Point(200, 74);
+            this.panelRight_CSR.Name = "panelRight_CSR";
+            this.panelRight_CSR.Size = new System.Drawing.Size(634, 337);
+            this.panelRight_CSR.TabIndex = 2;
+            // 
+            // splitterPanel_CSR
+            // 
+            this.splitterPanel_CSR.Location = new System.Drawing.Point(0, 0);
+            this.splitterPanel_CSR.Name = "splitterPanel_CSR";
+            this.splitterPanel_CSR.Size = new System.Drawing.Size(3, 337);
+            this.splitterPanel_CSR.TabIndex = 0;
+            this.splitterPanel_CSR.TabStop = false;
+            // 
+            // buttonHelp_CSR
+            // 
+            this.buttonHelp_CSR.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonHelp_CSR.Location = new System.Drawing.Point(747, 22);
+            this.buttonHelp_CSR.Name = "buttonHelp_CSR";
+            this.buttonHelp_CSR.Size = new System.Drawing.Size(75, 46);
+            this.buttonHelp_CSR.TabIndex = 0;
+            this.buttonHelp_CSR.Text = "Справка";
+            this.buttonHelp_CSR.UseVisualStyleBackColor = false;
+            this.buttonHelp_CSR.Click += new System.EventHandler(this.buttonHelp_CSR_Click);
+            // 
+            // buttonFile_CSR
+            // 
+            this.buttonFile_CSR.BackColor = System.Drawing.Color.SteelBlue;
+            this.buttonFile_CSR.Location = new System.Drawing.Point(666, 22);
+            this.buttonFile_CSR.Name = "buttonFile_CSR";
+            this.buttonFile_CSR.Size = new System.Drawing.Size(75, 46);
+            this.buttonFile_CSR.TabIndex = 1;
+            this.buttonFile_CSR.Text = "Открыть файл";
+            this.buttonFile_CSR.UseVisualStyleBackColor = false;
+            this.buttonFile_CSR.Click += new System.EventHandler(this.buttonFile_CSR_Click);
+            // 
+            // buttonDone_CSR
+            // 
+            this.buttonDone_CSR.BackColor = System.Drawing.Color.Green;
+            this.buttonDone_CSR.Location = new System.Drawing.Point(585, 22);
+            this.buttonDone_CSR.Name = "buttonDone_CSR";
+            this.buttonDone_CSR.Size = new System.Drawing.Size(75, 46);
+            this.buttonDone_CSR.TabIndex = 2;
+            this.buttonDone_CSR.Text = "Выполнить";
+            this.buttonDone_CSR.UseVisualStyleBackColor = false;
+            this.buttonDone_CSR.Click += new System.EventHandler(this.buttonDone_CSR_Click);
+            // 
+            // groupBoxTask_CSR
+            // 
+            this.groupBoxTask_CSR.Controls.Add(this.textBoxTask_CSR);
+            this.groupBoxTask_CSR.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxTask_CSR.Name = "groupBoxTask_CSR";
+            this.groupBoxTask_CSR.Size = new System.Drawing.Size(567, 62);
+            this.groupBoxTask_CSR.TabIndex = 3;
+            this.groupBoxTask_CSR.TabStop = false;
+            this.groupBoxTask_CSR.Text = "Условие:";
+            // 
+            // groupBoxResult_CSR
+            // 
+            this.groupBoxResult_CSR.Controls.Add(this.dataGridViewResult_CRS);
+            this.groupBoxResult_CSR.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxResult_CSR.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxResult_CSR.Name = "groupBoxResult_CSR";
+            this.groupBoxResult_CSR.Size = new System.Drawing.Size(200, 337);
+            this.groupBoxResult_CSR.TabIndex = 0;
+            this.groupBoxResult_CSR.TabStop = false;
+            this.groupBoxResult_CSR.Text = "Вывод данных:";
+            // 
+            // chartResult_CSR
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chartResult_CSR.ChartAreas.Add(chartArea3);
+            this.chartResult_CSR.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend3.Name = "Legend1";
+            this.chartResult_CSR.Legends.Add(legend3);
+            this.chartResult_CSR.Location = new System.Drawing.Point(3, 0);
+            this.chartResult_CSR.Name = "chartResult_CSR";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chartResult_CSR.Series.Add(series3);
+            this.chartResult_CSR.Size = new System.Drawing.Size(631, 337);
+            this.chartResult_CSR.TabIndex = 1;
+            this.chartResult_CSR.Text = "chart1";
+            // 
+            // textBoxTask_CSR
+            // 
+            this.textBoxTask_CSR.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBoxTask_CSR.Location = new System.Drawing.Point(6, 19);
+            this.textBoxTask_CSR.Multiline = true;
+            this.textBoxTask_CSR.Name = "textBoxTask_CSR";
+            this.textBoxTask_CSR.ReadOnly = true;
+            this.textBoxTask_CSR.Size = new System.Drawing.Size(555, 37);
+            this.textBoxTask_CSR.TabIndex = 0;
+            this.textBoxTask_CSR.Text = "Прочитать данные из файла InPutFileTask5V16.txt. Вывести в dataGridView. Вывести " +
+    "все положительные числа.Построить диаграмму по этим значениям.";
+            // 
+            // dataGridViewResult_CRS
+            // 
+            this.dataGridViewResult_CRS.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResult_CRS.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewResult_CRS.Location = new System.Drawing.Point(3, 16);
+            this.dataGridViewResult_CRS.Name = "dataGridViewResult_CRS";
+            this.dataGridViewResult_CRS.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridViewResult_CRS.Size = new System.Drawing.Size(194, 318);
+            this.dataGridViewResult_CRS.TabIndex = 0;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(834, 411);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelRight_CSR);
+            this.Controls.Add(this.panelLeft_CSR);
+            this.Controls.Add(this.panelTop_CSR);
             this.MinimumSize = new System.Drawing.Size(850, 450);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Спринт 6 | Таск 5 | Вариант 16 | Чепурных С. Р.";
+            this.panelTop_CSR.ResumeLayout(false);
+            this.panelLeft_CSR.ResumeLayout(false);
+            this.panelRight_CSR.ResumeLayout(false);
+            this.groupBoxTask_CSR.ResumeLayout(false);
+            this.groupBoxTask_CSR.PerformLayout();
+            this.groupBoxResult_CSR.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartResult_CSR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResult_CRS)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panelTop_CSR;
+        private System.Windows.Forms.Panel panelLeft_CSR;
+        private System.Windows.Forms.Panel panelRight_CSR;
+        private System.Windows.Forms.Splitter splitterPanel_CSR;
+        private System.Windows.Forms.GroupBox groupBoxTask_CSR;
+        private System.Windows.Forms.Button buttonDone_CSR;
+        private System.Windows.Forms.Button buttonFile_CSR;
+        private System.Windows.Forms.Button buttonHelp_CSR;
+        private System.Windows.Forms.GroupBox groupBoxResult_CSR;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartResult_CSR;
+        private System.Windows.Forms.TextBox textBoxTask_CSR;
+        private System.Windows.Forms.DataGridView dataGridViewResult_CRS;
     }
 }
 
--- Tyuiu.ChepurnykhSR.Sprint6.Task5.V16/FormMain.cs
+++ Tyuiu.ChepurnykhSR.Sprint6.Task5.V16/FormMain.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO;
+
+using Tyuiu.ChepurnykhSR.Sprint6.Task5.V16.Lib;
+
 namespace Tyuiu.ChepurnykhSR.Sprint6.Task5.V16
 {
     public partial class FormMain : Form
@@ -16,5 +20,36 @@ namespace Tyuiu.ChepurnykhSR.Sprint6.Task5.V16
         {
             InitializeComponent();
         }
-    }
+        DataService ds = new DataService();
+        string path = @"C:\Users\user\source\repos\Tyuiu.ChepurnykhSR.Sprint6\Tyuiu.ChepurnykhSR.Sprint6.Task5.V16\bin\Debug\InPutFileTask5V16.txt";
+        private void buttonDone_CSR_Click(object sender, EventArgs e)
+        {
+            dataGridViewResult_CRS.ColumnCount = 2;
+            dataGridViewResult_CRS.Columns[0].Width = 30;
+            dataGridViewResult_CRS.Columns[1].Width = 50;
+            this.chartResult_CSR.ChartAreas[0].AxisX.Title = "Ось X";
+            this.chartResult_CSR.ChartAreas[0].AxisY.Title = "Ось Y";
+            chartResult_CSR.Series[0].Points.Clear();
+            double[] numMass = new double[ds.len];
+            numMass = ds.LoadFromDataFile(path);
+            for (int i = 0; i < numMass.Length; i++)
+            {
+                dataGridViewResult_CRS.Rows.Add(Convert.ToString(i), Convert.ToString(numMass[i]));
+                chartResult_CSR.Series[0].Points.AddXY(i, numMass[i]);
+            }
+        }
+
+        private void buttonFile_CSR_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process txt = new System.Diagnostics.Process();
+            txt.StartInfo.FileName = "notepad.exe";
+            txt.StartInfo.Arguments = path;
+            txt.Start();
+        }
+
+        private void buttonHelp_CSR_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 5 выполнилf студентrf группы ИСТНб-23-1 Чепурных Софья Романовна", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }    
 }
